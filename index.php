80821f8ff851c2b6b31475816d3c79eb53927d8d
aatrayo <prattusighosh@gmail.com>
2023-05-20 01:09:30 +0530

    Create index.php
    creating listing of table

---
+++ index.php
@@ -0,0 +1,20 @@
+<table id="example" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th></th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Position</th>
+            <th>Office</th>
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <th></th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Position</th>
+            <th>Office</th>
+        </tr>
+    </tfoot>
+</table>
